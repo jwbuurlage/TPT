0efd6fbb090597cc09d9d37ccc3be4485ae129d9
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2016-07-28 10:06:48 +0200

    Add common type definitions to python bindings

---
+++ python/include/python_common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <boost/hana.hpp>
+
+namespace tomo {
+namespace python {
+
+namespace hana = boost::hana;
+using namespace hana::literals;
+
+using namespace std::string_literals;
+
+using T = double;
+
+} // namespace python
+} // namespace tomo
