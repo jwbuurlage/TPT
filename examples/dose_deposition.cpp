a539de41ab3317bc5b3d5ec2b461526065bc18ad
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2016-12-16 16:55:59 +0100

    Add external plotter to dose example

--- examples/dose_deposition.cpp
+++ examples/dose_deposition.cpp
@@ -1,29 +1,36 @@
 #include "tomo.hpp"
+#include "util/plotter.hpp"
 
 using T = float;
 
 int main() {
-    int k = 32;
-    auto v = tomo::volume<3_D>(k);
+    // request a plotter scene
+    auto plotter =
+        tomo::ext_plotter<2_D, T>("tcp://localhost:5555", "Test plot");
+
+    int k = 256;
+    auto v = tomo::volume<2_D>(k);
     auto f = tomo::modified_shepp_logan_phantom<T>(v);
-    auto g = tomo::geometry::cone_beam<T>(v, k, 1.0, {k, k}, 2.0, 2.0);
+    auto g = tomo::geometry::parallel<2_D, T>(k, k, v);
 
-    auto proj = tomo::dim::linear<3_D, T>(v);
-    auto dose = tomo::image<3_D, T>(v);
+    auto proj = tomo::dim::linear<2_D, T>(v);
+    auto dose = tomo::image<2_D, T>(v);
 
-    tomo::ascii_plot(f, 10, 1);
-
-    for (auto line : g) {
-        auto linear_attenuation = (T)0.0;
-        auto intensity = (T)1.0;
-        for (auto attenuation_point : proj(line)) {
-            auto factor = attenuation_point.value * f[attenuation_point.index];
-            linear_attenuation += factor;
-            auto new_intensity = tomo::math::exp(-linear_attenuation);
-            dose[attenuation_point.index] += intensity - new_intensity;
-            intensity = new_intensity;
+    for (int i = 0; i < 10; ++i) {
+        for (auto line : g) {
+            auto linear_attenuation = (T)0.0;
+            auto intensity = (T)1.0;
+            for (auto attenuation_point : proj(line)) {
+                auto factor =
+                    attenuation_point.value * f[attenuation_point.index];
+                linear_attenuation += factor;
+                auto new_intensity = tomo::math::exp(-linear_attenuation);
+                dose[attenuation_point.index] += intensity - new_intensity;
+                intensity = new_intensity;
+            }
         }
+        plotter.plot(dose);
     }
 
-    tomo::ascii_plot(dose, 10, 1);
+    return 0;
 }
