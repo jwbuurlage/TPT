49d424fce9f7811066dd6875796776f7480141cd
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2017-03-14 18:19:33 +0100

    Add downsampled volume data to external plotter

--- include/util/plotter.hpp
+++ include/util/plotter.hpp
@@ -5,7 +5,7 @@
 
 #include <zmq.hpp>
 
-#include "packets.hpp"
+#include "tomovis.hpp"
 
 #include "../common.hpp"
 #include "../image.hpp"
@@ -32,6 +32,51 @@ std::vector<unsigned char> pack_image(image<2_D, T> f) {
             (unsigned char)((T)255 * std::max(f[k], (T)0) / max);
     }
 
+    return grayscale_image;
+}
+
+template <typename T>
+std::vector<unsigned char> pack_image(image<3_D, T>& f,
+                                      std::vector<int> volume_size) {
+    assert(volume_size.size() == 3);
+
+    std::vector<unsigned char> grayscale_image(volume_size[0] * volume_size[1] *
+                                               volume_size[2]);
+
+    T max = (T)0;
+    for (int k = 0; k < f.get_volume().cells(); ++k) {
+        if (f[k] > max) {
+            max = f[k];
+        }
+    }
+
+    auto voxels = f.get_volume().voxels();
+    auto stride = math::vec3<T>{voxels[0] / (T)volume_size[0],
+                                voxels[1] / (T)volume_size[1],
+                                voxels[2] / (T)volume_size[2]};
+
+    std::cout << stride[0] << " " << stride[1] << " " << stride[2] << "\n";
+
+    int idx = 0;
+    for (int k = 0; k < volume_size[2]; ++k) {
+        for (int j = 0; j < volume_size[1]; ++j) {
+            for (int i = 0; i < volume_size[0]; ++i) {
+                grayscale_image[idx++] =
+                    (unsigned char)((T)255 * std::max(f({(int)(i * stride[0]),
+                                                         (int)(j * stride[1]),
+                                                         (int)(k)*stride[2]}),
+                                                      (T)0) /
+                                    max);
+                std::cout << idx << " " << (int)grayscale_image[idx] << " "
+                          << f({(int)(i * stride[0]), (int)(j * stride[1]),
+                                (int)(k)*stride[2]})
+                          << "\n";
+            }
+        }
+    }
+
+    std::cout << "10 = " << (int)grayscale_image[10] << " / max = " << max
+              << "\n";
     return grayscale_image;
 }
 
@@ -115,7 +160,8 @@ class ext_plotter<3_D, T> : public ext_plotter_base<3_D> {
 
     void plot(tomo::image<3_D, T> f) {
         for (int axis = 0; axis < 3; ++axis) {
-            auto slice = tomo::slice(f, f.get_volume().voxels()[axis] / 2, axis);
+            auto slice =
+                tomo::slice(f, f.get_volume().voxels()[axis] / 2, axis);
 
             std::vector<int> image_size(2_D);
             for (int d = 0; d < 2_D; ++d) {
@@ -130,6 +176,18 @@ class ext_plotter<3_D, T> : public ext_plotter_base<3_D> {
             zmq::message_t reply;
             socket_.recv(&reply);
         }
+
+        // lets also send low resoution volume data
+        int downsample_size = 32;
+        std::vector<int> volume_size(3, downsample_size);
+
+        auto vol_packet = tomovis::VolumeDataPacket(
+            scene_id_, volume_size, std::move(pack_image(f, volume_size)));
+
+        vol_packet.send(socket_);
+
+        zmq::message_t reply;
+        socket_.recv(&reply);
     }
 };
 
