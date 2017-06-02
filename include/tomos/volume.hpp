1278d20a6f76f83f3af5aa588115069e311de8b4
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2017-06-02 16:45:45 +0200

    Initial work on simplified distributed reconstruction

--- include/tomos/volume.hpp
+++ include/tomos/volume.hpp
@@ -8,23 +8,6 @@
 
 namespace tomo {
 
-template <bool...>
-struct bool_pack {};
-
-template <bool... bs>
-using all_true = std::is_same<bool_pack<bs..., true>, bool_pack<true, bs...>>;
-
-template <class R, class... Ts>
-using are_all_convertible = all_true<std::is_convertible<Ts, R>::value...>;
-
-template <int count, class R, class... Ts>
-using count_of_type =
-    all_true<sizeof...(Ts) == count, are_all_convertible<R, Ts...>::value>;
-
-template <int D, typename... Ts>
-using check_dim =
-    typename std::enable_if<count_of_type<D, int, Ts...>::value>::type;
-
 /**
  * The region which is being imaged.
  *
